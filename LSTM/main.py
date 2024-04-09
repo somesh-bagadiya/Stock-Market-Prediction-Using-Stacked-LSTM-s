c41131fff45abd10941e0e85482bf52abca4b28c
Somesh Bagadiya <someshbgd3@gmail.com>
2024-04-09 14:16:13 -0700

    Visualization Changes

--- LSTM/main.py
+++ LSTM/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import math
 from sklearn.metrics import mean_squared_error
 import time
+import tensorflow as tf
 
 start_time = time.time()
 
@@ -42,13 +43,15 @@ X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
 model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(100, 1)))
-model.add(LSTM(50, return_sequences=True))
-model.add(LSTM(50))
+model.add(LSTM(100, return_sequences=True, input_shape=(100, 1)))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100, return_sequences=True))
+model.add(LSTM(100))
 model.add(Dense(1))
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 model.compile(loss='mean_squared_error', optimizer='adam')  # Stochastic Gradient Descent
 model.summary()
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, verbose=1)
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, verbose=1)
 
 training_time = time.time() - start_time
 print("Training Time:", training_time)
@@ -67,6 +70,8 @@ loss = model.evaluate(X_test, y_test)
 a = math.sqrt(mean_squared_error(y_train, train_predict))
 b = math.sqrt(mean_squared_error(y_test, test_predict))
 print("Train Error", a,"Test Error:", b)
+
+#%%
 
 look_back = 100
 trainPredictPlot = np.empty_like(data)
@@ -95,3 +100,59 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
+
+orig_data = np.array(df['Close'])
+orig_data = orig_data[time_step+1 :training_size]
+train_predict = np.reshape(train_predict, orig_data.shape)
+x_axis1 = np.arange(len(train_predict))
+train_diff = orig_data - train_predict
+mask1 = train_diff < 0   
+
+orig_data = np.array(df['Close'])
+orig_data = orig_data[training_size + time_step+1 : ]
+test_predict = np.reshape(test_predict, orig_data.shape)
+x_axis2 = np.arange(len(test_predict))
+test_diff = orig_data - test_predict
+mask2 = test_diff < 0   
+
+
+#%%
+
+plt.plot(train_diff)
+plt.plot(x_axis1[mask1], train_diff[mask1], 'ro', markersize=2, label='Predicted > Actual')
+plt.plot(x_axis1[~mask1], train_diff[~mask1], 'bo', markersize=2, label='Predicted < Actual')
+plt.xlabel('Day')
+plt.ylabel('Value Difference')
+plt.legend()
+plt.title('Plotting difference between Orignal Values and Training predictions')
+plt.show()
+
+plt.plot(test_diff)
+plt.plot(x_axis2[mask2], test_diff[mask2], 'ro', markersize=2, label='Predicted > Actual')
+plt.plot(x_axis2[~mask2], test_diff[~mask2], 'bo', markersize=2, label='Predicted < Actual')
+plt.xlabel('Day')
+plt.ylabel('Value Difference')
+plt.legend()
+plt.title('Plotting difference between Orignal Values and Testing predictions')
+plt.show()
+
+#%%
+
+# Dumb Baseline
+orig_data = df["Close"]
+orig_data = orig_data[time_step+1 :training_size].tolist()
+val1 = [0]*len(orig_data)
+val2 = [0]*len(orig_data)
+for i in range(len(orig_data)):
+    if i+1 != len(orig_data):
+        val1[i] = orig_data[i]
+        val2[i] = orig_data[i+1]
+        
+print(mean_squared_error(val1[:-1], val2[:-1]))
+
+orig_data = np.array(df['Close'])
+orig_data = orig_data[time_step+1 :training_size]
+train_predict = np.reshape(train_predict, orig_data.shape)
+
+print(mean_squared_error(orig_data, train_predict))
+
